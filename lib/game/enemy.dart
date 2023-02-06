9ad118d09288ab0a6449bc1b74fd5b7265fde910
AbhishekSatpathy4848 <abhisheksatpathy.211it002@nitk.edu.in>
2023-02-06 19:26:04 +0530

    added enemy component

---
+++ lib/game/enemy.dart
@@ -0,0 +1,70 @@
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
+import 'package:dino_game/core/constants.dart';
+
+enum EnemyType {
+  angryPig,
+  bat,
+}
+
+class EnemyData {
+  final String spriteSheetPath;
+  final int totalSprites;
+  final double spriteFrameDurations;
+  final Vector2 textureSize;
+  final Vector2 speed;
+  final double enemySpritePadding;
+
+  const EnemyData(
+      {required this.spriteSheetPath,
+      required this.totalSprites,
+      required this.spriteFrameDurations,
+      required this.textureSize,
+      required this.speed,
+      required this.enemySpritePadding});
+}
+
+Map<EnemyType, EnemyData> enemyMap = {
+  EnemyType.angryPig: EnemyData(
+    spriteSheetPath: 'AngryPig.png',
+    totalSprites: 16,
+    spriteFrameDurations: frameDuration,
+    textureSize: Vector2(36, 36),
+    speed: Vector2(-200, 0),
+    enemySpritePadding: 16,
+  ),
+  EnemyType.bat: EnemyData(
+    spriteSheetPath: 'Bat.png',
+    totalSprites: 7,
+    spriteFrameDurations: frameDuration,
+    textureSize: Vector2(46, 46),
+    speed: Vector2(-250, 0),
+    enemySpritePadding: 10,
+  ),
+};
+
+class Enemy extends SpriteAnimationComponent{
+  double xMax;
+  EnemyType enemyType;
+
+  Enemy({required this.enemyType, required this.xMax}) : super() {
+    SpriteAnimationData spriteAnimationData = SpriteAnimationData.sequenced(
+      amount: enemyMap[enemyType]!.totalSprites,
+      stepTime: enemyMap[enemyType]!.spriteFrameDurations,
+      textureSize: enemyMap[enemyType]!.textureSize,
+    );
+    Flame.images.load(enemyMap[enemyType]!.spriteSheetPath).then((spriteSheet) {
+      SpriteAnimation spriteAnimation =
+          SpriteAnimation.fromFrameData(spriteSheet, spriteAnimationData);
+      animation = spriteAnimation;
+      return this;
+    });
+  }
+
+  @override
+  void update(double dt) {
+    x += enemyMap[enemyType]!.speed[0] * dt;
+    if (x <= -enemyMap[enemyType]!.textureSize[0]) x = xMax;
+    super.update(dt);
+  }
+}
